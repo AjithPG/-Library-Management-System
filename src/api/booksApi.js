e1d317c179c26594498f739d016af7d9caf5a17c
ajithpg <ajithpg008@gmail.com>
2024-09-07 20:17:40 +0530

    supbase configuration'

---
+++ src/api/booksApi.js
@@ -0,0 +1,14 @@
+
+export const getBooks = async()=>{
+    const { data: books, error } = await supabase
+    .from('books')
+    .select('*')
+
+    if(error){
+        console.log(error)
+        throw new Error('Error while getting list of books. Try again later')
+    }
+
+    return book
+}
+
